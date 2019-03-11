4bc663df355f05d0c914adc01e94cbe244ec3543
louisroman <louisroman133@gmail.com>
2019-03-11 12:25:43 -0400

    inital commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./scss/App.scss";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quae
+          repudiandae fugit, dolor minus accusamus quos placeat exercitationem
+          illum earum facere esse. Quisquam fuga itaque molestiae voluptas quia
+          magnam possimus quaerat! Ducimus perspiciatis quam modi perferendis.
+          Officia quasi doloremque nam vitae voluptate optio ducimus expedita,
+          aut fuga, ea amet nemo at? Illo suscipit expedita, perferendis eius
+          voluptas magnam molestias reprehenderit odio consequatur nesciunt
+          dolorum iste aut perspiciatis dolore, corporis facere sed quasi
+          cupiditate dolores voluptatum doloribus! Distinctio amet omnis ea
+          animi consequatur dolor fugit veritatis, quas reiciendis possimus! Hic
+          mollitia ipsum, rerum recusandae doloribus iure consequatur pariatur
+          dolores. Aperiam, id!
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
