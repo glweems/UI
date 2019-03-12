ada59022bfa71ccd6d397dce6081eaff67fcbbf4
Garrett Weems <gwgraphicdesign@gmail.com>
2019-03-12 18:43:09 -0400

    styled-components theme

---
+++ src/Theme.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const HeadingDisplay = styled.h1`
+  font-weight: black;
+  font-style: italic;
+`;
+export default {
+  colors: {
+    red: '#e80310',
+    yellow: '#F8DC51',
+    darkYellow: '#EBD150',
+  },
+};
