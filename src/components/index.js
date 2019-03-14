8ab9baf305d6fd5c8e60165453b0c5123a0220b6
Garrett Weems <gwgraphicdesign@gmail.com>
2019-03-13 20:00:13 -0400

    updates

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// const pokedexUrl = require('../images/pokedex.svg');
+
+export const Container = styled.div`
+  max-width: 90%;
+  margin: 0 auto;
+  * {
+    max-width: 90%;
+    margin: 0 auto;
+  }
+`;
+
+export default { Container };
